fa259d1f13bff9c65a774c9fa955b396868e4df7
Senjerak <gabriela.lecusay001@mymdc.net>
2022-12-09 17:10:31 -0500

    loon

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var $body = $('body');
+
+$('html').on('keydown', function (e) {
+    var $bubble;
+
+    if (e.key == ' ') {
+        $bubble = $('<div>');
+        $bubble.addClass('bubble');
+        $bubble.css({
+            'top': Math.random() * (document.documentElement.clientHeight - 100),
+            'left': Math.random() * (document.documentElement.clientWidth - 100)
+        });
+        $body.append($bubble);
+    }
+});
+
+const audioElement = $('<audio>')[0];
+audioElement.setAttribute('src', './media/loooon.mp4')
+
+
+$body.on('click', '.bubble', function () {
+    $(this).addClass('is-popping');
+    audioElement.play();
+
+});
+
+$body.on('transitioned', '.bubble', function () {
+    $(this).remove();
+}
+)
